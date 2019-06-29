8dc50786e03442bdc2c4083e4fec7efda3bd0eea
florisweb <github@florisweb.tk>
2019-06-29 16:07:49 +0200

    Another test update

--- images/sideBarBackground/generator.php
+++ images/sideBarBackground/generator.php
@@ -42,7 +42,7 @@
 				
 			// }
 			
-			let canvas = createCanvasBackground(Colors, 700, 150, 10);
+			let canvas = createCanvasBackground(Colors, 80, 80, 10);
 			document.body.append(canvas);
 
 
