5b21863df3c693308a945d2ec422240cc3c61fb9
florisweb <github@florisweb.tk>
2019-07-24 13:46:24 +0200

    Added backend and frontend changeProjectTitle support, it doesn't have UI yet though

---
+++ database/project/changeProjectTitle.php
@@ -0,0 +1,14 @@
+<?php
+	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+	require_once "$root/git/todo/database/modules/app.php";
+
+	$_projectId 	= (String)$_POST["projectId"];
+	$_newTitle	 	= urldecode((String)$_POST["newTitle"]);
+	$project 		= $App->getProject($_projectId);
+	
+	if (!$_newTitle)			die("E_noTitleProvided");
+	if (!$project) 				die("E_projectNotFound");
+	if (is_string($project)) 	die($project);
+
+	echo $project->changeTitle($_newTitle);
+?>
