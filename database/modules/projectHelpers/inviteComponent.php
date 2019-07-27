ef7a609d492cf77b8e7916ea22c402afc17682ff
florisweb <github@florisweb.tk>
2019-07-27 17:37:17 +0200

    Redesigned the invite-email

--- database/modules/projectHelpers/inviteComponent.php
+++ database/modules/projectHelpers/inviteComponent.php
@@ -113,19 +113,26 @@
 			return preg_replace("/[^0-9,^a-z,^A-Z,.,@,,]/", "", $_emailAdress);;
 		}
 
+
+
 		private function sendMail($_emailAdress, $_inviteId) {
 			$senderName = $this->Parent->get(
 				$GLOBALS["App"]->userId
 			)["name"];
 			if (!is_string($senderName)) return false;
+			if (!$senderName) $senderName = "A nameless user";
+
+			$projectTitle 	= htmlspecialchars($this->Project->title);
+			$senderName 	= htmlspecialchars($senderName);
+
 
 			$inviteLink = "https://florisweb.tk/git/todo/invite?id=" . $_inviteId;
-		    $html = "<html><head></head><body style='background-color: #eee; text-align: center'>Hey there $inviteLink</body></html>";
+		    $html = '<!DOCTYPE html><html><head><meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" name="viewport"/><style>body{margin:0;padding:0;height:100vh;overflow:hidden;background:linear-gradient(to bottom right, rgb(221, 157, 255), rgb(154, 191, 240))}#backgroundImage{position:fixed;left:0;top:0;width:100vw;height:auto;animation:backgroundImage_popIn 1s 1;animation-delay:0s;animation-fill-mode:forwards;animation:backgroundImage_passive 5s 10000}@media (max-aspect-ratio: 7/4){#backgroundImage{width:auto;height:100vh}}@keyframes backgroundImage_popIn{0%{opacity:0}100%{opacity:1;transform:scale(1.1)}}@keyframes backgroundImage_passive{0%{opacity:0.5}50%{opacity:1;transform:scale(1.05)}100%{opacity:0.5}}#menuHolder{position:relative;margin:auto;width:calc(90vw);max-width:480px;height:100vh}.inviteMenu{position:absolute;top:calc(50vh - 150px);width:calc(100% - 40px * 2);padding:40px;background:#fff;border-radius:3px;box-shadow:5px 5px 15px 15px rgba(0, 0, 0, 0.01);transition:top 0.3s;animation:inviteMenu_popIn 0.7s 1;animation-delay:0.1s;animation-fill-mode:forwards;transform:scale(0);opacity:0;margin-top:20vh}@keyframes inviteMenu_popIn{0%{transform:scale(0);opacity:0;margin-top:20vh}100%{margin-top:0;opacity:1;transform:scale(1)}}#menu_linkFound{top:calc(50vh - 200px);padding-bottom:20px}.text{font-family:"Lucida Sans Unicode", "Lucida Grande", sans-serif;color:#999;font-size:15px;line-height:25px}.text.tHeaderLarge{font-size:20px;color:#888}.text.highlight{font-weight:bold}.button{position:relative;width:auto;padding:7px 15px;border-radius:4px;font-size:13px;cursor:pointer;background:rgb(140, 153, 247);color:#fff;text-align:center}</style></head>	<body><img src="https://florisweb.tk/git/todo/images/sideBarBackground/backgrounds/fullScreen.jpg" id="backgroundImage"><div id="menuHolder"><div class="inviteMenu" id="menu_linkFound"><div class="text tHeaderLarge">' . $projectTitle . '</div><br><br><div class="text">' . $senderName . ' has invited you to join their veratio project <a class="text highlight">' . $projectTitle . '</a>.</div><br><br><br><a href="' . $inviteLink . '" style="text-decoration: none"><div class="button text">Join</div></a></div></div></body></html>';
 		
 			$headers =  "From: veratio@florisweb.tk" . "\r\n" . 
            				"MIME-Version: 1.0" . "\r\n" . 
 			           	"Content-type: text/html; charset=UTF-8" . "\r\n";
-			mail($_emailAdress, "You've been invited to " . $this->Project->title . " by " . $senderName, $html, $headers);
+			mail($_emailAdress, "You've been invited to " . $projectTitle . " by " . $senderName . " - veratio", $html, $headers);
 			return true;
 		}
 
