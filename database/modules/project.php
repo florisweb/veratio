431a29ca3cef56d85c2ddd2c5db2573e21a8dcca
florisweb <github@florisweb.tk>
2020-03-23 17:59:30 +0100

    Implemented the new permissionsystem for the main-project-features

--- database/modules/project.php
+++ database/modules/project.php
@@ -32,9 +32,7 @@
 			$projectData = $this->DB->getProjectData();
 			if (!$projectData) return $this->errorOnCreation = "E_projectNotFound";
 
-
-			$user = $this->users->get($GLOBALS["App"]->userId);
-			if (!$user || $user["id"] != $GLOBALS["App"]->userId) return $this->errorOnCreation = "E_userNotInProject";
+			if (!$this->users->Self) return $this->errorOnCreation = "E_userNotInProject";
 
 			$this->title = $projectData["title"];
 			$this->errorOnCreation = false;
@@ -42,11 +40,10 @@
 
 
 		public function rename($_newTitle = "Titleless") {
-			$user = $this->users->get($GLOBALS["App"]->userId);
+			$user = $this->users->Self;
 			if (!$user) return "E_userNotInProject";
 
-			$userPermissions = json_decode($user["permissions"], true);
-			if ($userPermissions[3] < 1) return "E_actionNotAllowed";
+			if ((int)$user["permissions"] < 2) return "E_actionNotAllowed";
 
 			return $this->DB->writeProjectData("title", (string)$_newTitle);
 		}
@@ -54,11 +51,10 @@
 
 
 		public function remove() {
-			$user = $this->users->get($GLOBALS["App"]->userId);
+			$user = $this->users->Self;
 			if (!$user) return "E_userNotInProject";
 
-			$userPermissions = json_decode($user["permissions"], true);
-			if ($userPermissions[3] < 2) return "E_actionNotAllowed";
+			if ((int)$user["permissions"] < 3) return "E_actionNotAllowed";
 
 			return $this->DB->removeProject();			
 		}
