00ee23e6f5383e633b139794ba590c9b703326b7
florisweb <github@florisweb.tk>
2020-07-17 09:53:46 +0200

    Added the getProjectIdList-function to the indexedDB-script

--- js/serviceWorker/indexedDB.js
+++ js/serviceWorker/indexedDB.js
@@ -18,10 +18,6 @@ const LocalDB = new function() {
     request.onupgradeneeded = function(_e) { // create object stores
       DB = _e.target.result;
 
-      // const metaData  = DB.createObjectStore("metaData", {keyPath: "projectId"});
-      // const tasks     = DB.createObjectStore("tasks", {keyPath: "projectId"});
-      // const users     = DB.createObjectStore("users", {keyPath: "projectId"});
-      // const tags      = DB.createObjectStore("tags",  {keyPath: "projectId"});
       const metaData  = DB.createObjectStore("metaData");
       const tasks     = DB.createObjectStore("tasks");
       const users     = DB.createObjectStore("users");
@@ -38,13 +34,29 @@ const LocalDB = new function() {
   }
 
 
+  this.getProjectIdList = getProjectIdList;
+  function getProjectIdList() {
+    return new Promise(function (resolve, error) {
+      let store = DB.transaction("metaData", "readonly").objectStore("metaData");
 
-  this.getProject = function(_projectId) {
-    return new LocalDB_Project(_projectId, DB);
+      let ids = [];
+      store.getAll().onsuccess = function(_e) {
+        if (!_e.target.result) return error();
+
+        resolve(
+          _e.target.result.map(function (_item) {return _item.id})
+        );
+      };
+    });
   }
 
 
 
+
+
+  this.getProject = function(_projectId) {
+    return new LocalDB_Project(_projectId, DB);
+  }
 }
 
 
@@ -82,14 +94,14 @@ function LocalDB_Project(_projectId, _DB) {
       transaction.onerror = error;
       const store = transaction.objectStore(_key);
 
+      _value.id = This.id;
       let trans2 = store.put(_value, This.id);
       trans2.transaction.onsuccess = resolve;
     });
   }
-
-
 }
 //let project = LocalDB.getProject("testId2");
+//let project2 = LocalDB.getProject("testId3");
 
 //setData("metaData", {title: "testProject"});
 
