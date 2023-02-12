38eb5c991217cc3a6ece987d6196da32e9d5c4db
florisweb <floris@florisweb.dev>
2023-02-12 17:10:35 +0100

    Implemented rendering of tasks in the planner and added some styling.

--- src/js/mainContent/pages/plannerPage.js
+++ src/js/mainContent/pages/plannerPage.js
@@ -2,6 +2,8 @@
 
 
 class MainContent_plannerPage {
+	#dayHeightScrollMargin = 5 * 100; // 100 = height element
+
 	HTML = {};
 	constructor() {
 		MainContent_page.call(this, {
@@ -21,13 +23,14 @@ class MainContent_plannerPage {
 		MainContent.header.setMemberList([]);
 
 		this.render();
+		this.HTML.infiniteScrollHolder.scrollTop = this.#dayHeightScrollMargin;
 	}
 
 
 	#UIDays = [];
 	async render() {
 		let date = new Date();
-		let firstDateOfWeek = date.moveDay(-date.getDay() + 1 - 7); // 0: sunday -> 1: monday
+		let firstDateOfWeek = date.moveDay(-date.getDay() + 1 - 7 * (5 + 1)); // 0: sunday -> 1: monday
 
 		this.#firstWeekDate = firstDateOfWeek;
 		for (let i = 0; i < 20; i++)
@@ -41,28 +44,40 @@ class MainContent_plannerPage {
 
 	#firstWeekDate = new Date(); // the monday of
 	#lastWeekDate = new Date(); // the monday of
-	renderWeekAtStart(_firstDay) {
+	async renderWeekAtStart(_firstDay) {
 		this.#firstWeekDate = _firstDay;
+		let UIDays = [];
 		for (let i = 6; i >= 0; i--)
 		{
 			let curDate = _firstDay.copy().moveDay(i);
-			let UIDay = new PlannerPage_UIDay({date: curDate, tasks: []});
-			this.HTML.dayHolder.insertBefore(UIDay.HTML, this.HTML.dayHolder.children[0]);
+			UIDays[i] = new PlannerPage_UIDay({date: curDate, tasks: []});
+			this.HTML.dayHolder.insertBefore(UIDays[i].HTML, this.HTML.dayHolder.children[0]);
 		}
+
+		let taskList = await Server.accessPoints.weekTab.getTasksByDateRange({date: _firstDay, range: 7}, true);
+		let tasksByDate = splitTasksByDate(TaskSorter.defaultSort(taskList));
+		for (let day of UIDays) day.setTasks(tasksByDate[day.date.toString()]);
 	}
 
 	removeWeekAtStart() {
 		for (let i = 0; i < 7; i++) this.HTML.dayHolder.removeChild(this.HTML.dayHolder.children[0]);
+		this.#firstWeekDate = this.#firstWeekDate.moveDay(7);
 	}
 
-	renderWeekAtEnd(_firstDay) {
+	async renderWeekAtEnd(_firstDay) {
 		this.#lastWeekDate = _firstDay;
+		let UIDays = [];
 		for (let i = 0; i < 7; i++)
 		{
 			let curDate = _firstDay.copy().moveDay(i);
-			let UIDay = new PlannerPage_UIDay({date: curDate, tasks: []});
-			this.HTML.dayHolder.append(UIDay.HTML);
+			UIDays[i] = new PlannerPage_UIDay({date: curDate, tasks: []});
+			this.HTML.dayHolder.append(UIDays[i].HTML);
 		}
+
+		let taskList = await Server.accessPoints.weekTab.getTasksByDateRange({date: _firstDay, range: 7}, true);
+		let tasksByDate = splitTasksByDate(TaskSorter.defaultSort(taskList));
+		console.log(tasksByDate);
+		for (let day of UIDays) day.setTasks(tasksByDate[day.date.toString()]);
 	}
 	
 	removeWeekAtEnd() {
@@ -70,14 +85,14 @@ class MainContent_plannerPage {
 		{
 			this.HTML.dayHolder.removeChild(this.HTML.dayHolder.children[this.HTML.dayHolder.children.length - 7]);
 		}
+		this.#lastWeekDate = this.#lastWeekDate.moveDay(-7);
 	}
 
 
 	#onScroll() {
-		const dayHeightScrollMargin = 5 * 100; // 100 = height element
 		let scrollY = this.HTML.infiniteScrollHolder.scrollTop;
-		let scrollFromTop = scrollY - dayHeightScrollMargin;
-		let scrollFromBottom = this.HTML.dayHolder.offsetHeight - scrollY - this.HTML.infiniteScrollHolder.offsetHeight - dayHeightScrollMargin;
+		let scrollFromTop = scrollY - this.#dayHeightScrollMargin;
+		let scrollFromBottom = this.HTML.dayHolder.offsetHeight - scrollY - this.HTML.infiniteScrollHolder.offsetHeight - this.#dayHeightScrollMargin;
 
 		if (scrollFromTop < 0)
 		{
@@ -90,8 +105,6 @@ class MainContent_plannerPage {
 		}
 
 		if (scrollY === 0) wait(0).then(() => {this.HTML.infiniteScrollHolder.scrollTop++; this.#onScroll()});
-
-		console.log('scroll', scrollFromTop, scrollFromBottom);
 	}
 
 }
@@ -109,12 +122,14 @@ class PlannerPage_UIDay {
 		this.date = date;
 		this.#HTML.self = createElement('div', 'UIDayElement');
 		if (this.date.getDay() === 0 || this.date.getDay() === 6) this.#HTML.self.classList.add('weekend');
-
+		if (this.date.getDateInDays(true) < new Date().getDateInDays(true)) this.#HTML.self.classList.add('inPast');
 		this.#HTML.self.innerHTML = `
 			<div class='text dateHolder'></div>
+			<div class='text taskHolder'></div>
 		`;
 
 		this.#HTML.dateHolder = this.#HTML.self.children[0];
+		this.#HTML.taskHolder = this.#HTML.self.children[1];
 		let title = this.date.getDate()
 		if (this.date.getDate() === 1) title += ' ' + this.date.getMonths()[this.date.getMonth()].name.substr(0, 3);
 		setTextToElement(this.#HTML.dateHolder, title);
@@ -123,4 +138,25 @@ class PlannerPage_UIDay {
 	get HTML() {
 		return this.#HTML.self;
 	}
+
+
+	setTasks(_tasks = []) {
+		this.#HTML.taskHolder.innerHTML = '';
+		for (let i = 0; i < Math.min(_tasks.length, 4); i++)
+		{
+			let curTask = _tasks[i];
+			let taskHolder = createElement('div', 'taskPreview text');
+			setTextToElement(taskHolder, curTask.title);
+			if (curTask.tagId) 
+			{
+				let tag = curTask.project.tags.get(curTask.tagId);
+				if (tag) taskHolder.style.color = tag.colour.toHex();
+			}
+			this.#HTML.taskHolder.append(taskHolder);
+		}
+	}
 }
+
+
+
+
